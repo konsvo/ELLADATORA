b02a717917f6135e4fa44a26cb7feb63823c2b11
konsvo <konsvo@gmail.com>
2026-09-04 00:08:46 +0300

    Use canonical www domain in app/robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://elladatora.gr/sitemap.xml" };
+  return { rules: { userAgent: "*", allow: "/" }, sitemap: "https://www.elladatora.gr/sitemap.xml" };
 }
